58d393b759135c62f4140bee9a3033f16a0b697e
CoolFroggy <60703328+CoolFroggy@users.noreply.github.com>
2020-02-05 16:37:22 +0100

    init

---
+++ HideBlockedMessages.plugin.js
@@ -0,0 +1,47 @@
+//META { "name": "HideBlockedMessages" } *//
+global.HideBlockedMessages = function () {
+	class HideBlockedMessages
+	{
+		getName() {
+		  return "Hide Blocked Messages";
+		}
+
+		getDescription() {
+		  return "Compatible with new Discord update begin feb 2020.";
+		}
+
+		getAuthor() {
+		  return "CoolFroggy";
+		}
+
+		getVersion() {
+		  return "1";
+		}
+		
+		load() {
+		}
+		
+		async start()
+		{
+			$(document).ready(
+				function() {
+					$('.groupStart-23k01U').has('.container-3FojY8').has('.blockedSystemMessage-2Rk1ek').css('display','none');
+				}
+			);
+			return document.addEventListener("scroll", onLoad, true);
+		}
+		
+		stop()
+		{
+			$('.groupStart-23k01U').has('.container-3FojY8').has('.blockedSystemMessage-2Rk1ek').css('display','grid');
+			return document.removeEventListener("scroll", onLoad, true);
+		}
+		
+	};
+	
+	onLoad = function (event) {
+		$('.groupStart-23k01U').has('.container-3FojY8').has('.blockedSystemMessage-2Rk1ek').css('display','none');
+	}
+	
+	return HideBlockedMessages;
+}.call(this);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HideBlockedMessages
+Download the .js file and place it in your plugins folder located at Discord settings > Plugins > Open Plugin Folder.
